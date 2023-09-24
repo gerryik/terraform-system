ed762f65f40f96944f18ba49b4b5ffb02e618ed1
gerryik <33073924+gerryik@users.noreply.github.com>
2023-09-25 00:47:00 +0100

    Update main.tf

--- target/systems/ec2/main.tf
+++ target/systems/ec2/main.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
+#provider "aws" {
+ # region = "eu-central-2"
+#}
+
+module "server" {
+  source = "https://github.com/gerryik/terraform-base-iac/tree/main//modules/ec2"
+  num = 2
+  region = "eu-central-2"
+}
